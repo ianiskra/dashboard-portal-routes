da915fe2128ca3daa002f2bbe829cd9a6c9c7df5
Ian Iskra <iiskra@BendPak.com>
2022-07-04 01:54:31 -0700

    Setup the building blocks for at ProtectedRoute

--- src/Authenticated/ProtectedRoute/ProtectedRoute.jsx
+++ src/Authenticated/ProtectedRoute/ProtectedRoute.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import { Outlet, Navigate } from 'react-router-dom';
 
-export const ProtectedRoute = () => {
+// The redirectPath is redirecting them to the login page
+
+/*
+Prop is the user,
+redirectPath is redirecting them to the login
+
+How do I get passed the authentication now?
+https://www.robinwieruch.de/react-router-private-routes/
+*/
+
+
+export const ProtectedRoute = ({
+    user,
+    redirectPath = '/login',
+}) => {
+
+    if (!user) {
+        return <Navigate to={redirectPath} replace />;
+    }
+
     return (
         <div>
             <h1>Protected Route</h1>
